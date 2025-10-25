2706a9ec2ceade885dc7334f7b36c4bc5ca962ca
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-10-25 18:39:22 +0530

    first commit

---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import {dummyResumeData} from '../assets/assets'
+import { ArrowLeftIcon, User, FileText, Briefcase, FolderIcon, GraduationCap, Sparkles} from 'lucide-react'
+import { useEffect } from 'react' 
+
+const ResumeBuilder = () => {
+
+  const { resumeId } = useParams()
+
+  const [resumeData, setResumeData] =  useState({
+    _id: '',
+    title: '',
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    skills: [],
+    projects: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false
+  })
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find(resume => resume._id === resumeId)
+
+    if(resume) {
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  const sections = [
+    { id: 'personal_info', name: 'Personal Info', icon: User },
+    { id: 'summary', name: 'Summary', icon: FileText },
+    { id: 'experience', name: 'Experience', icon: Briefcase },
+    { id: 'education', name: 'Education', icon: GraduationCap},
+    { id: 'projects', name: 'Projects', icon: FolderIcon },
+    { id: 'skills', name: 'Skills', icon: Sparkles },
+  ]
+  
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  const activeSection = sections[activeSectionIndex];
+
+  useEffect(() => {
+    loadExistingResume()
+  },[])
+
+  return (
+    <div>
+      <div className='max-w-7xl mx-auto px-4 py-6'>
+        <Link to={'/app'} className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all'>
+          <ArrowLeftIcon className="size-4"/> Back to Dashboard
+        </Link>
+      </div>
+
+      <div className='max-w-7xl mx-auto px-4 py-8'>
+        <div className='grid lg:grid-cols-12 gap-8'>
+          <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+            <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200"/>
+              <hr className='absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-2000' style={{width:`${activeSectionIndex * 100 / (sections.length - 1)}%`}}/>
+
+            </div>
+          </div>
+          <div></div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default ResumeBuilder
