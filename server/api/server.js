4528e3e605069bcb1f0adc1f6ef9dda4d78df29b
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-11-29 16:09:23 +0530

    changes in server.js

--- server/api/server.js
+++ server/api/server.js
@@ -1,16 +1,16 @@
 import express from "express";
-import cors from "cors";
 import serverless from "serverless-http";
-import "dotenv/config.js";
-import connectDB from "./configs/db.js";
-import userRouter from "./routes/userRoutes.js";
-import resumeRouter from "./routes/resumeRoutes.js";
-import aiRouter from "./routes/aiRoutes.js";
+import cors from "cors";
+import "../../server/configs/db.js";
+import connectDB from "../configs/db.jsnfigs/db.js";
+import userRouter from "../routes/userRoutes.jserRoutes.js";
+import resumeRouter from "../routes/resumeRoutes.jsmeRoutes.js";
+import aiRouter from "../routes/aiRoutes.jsaiRoutes.js";
+
+// Connect DB
+await connectDB();
 
 const app = express();
-
-// Database
-await connectDB();
 
 app.use(express.json());
 app.use(
@@ -22,13 +22,11 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("Backend working on Vercel!");
+  res.send("Backend is live on Vercel!");
 });
 
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
 app.use("/api/ai", aiRouter);
 
-// ❗ No app.listen() in Vercel
-export const handler = serverless(app);
-export default app;
+export default serverless(app);
