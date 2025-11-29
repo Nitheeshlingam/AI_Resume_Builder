6f287ebc6df65c3e45205647b32d9005002de552
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-11-29 10:15:12 +0530

    added cors updation in backend

--- server/server.js
+++ server/server.js
@@ -10,17 +10,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database Connection
-await connectDB()
+await connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://sparcv-client.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.get('/', (req,res)=> res.send("Server is live... "))
-app.use('/api/users', userRouter)
-app.use('/api/resumes', resumeRouter)
-app.use('/api/ai', aiRouter)
+app.get("/", (req, res) => res.send("Server is live... "));
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
