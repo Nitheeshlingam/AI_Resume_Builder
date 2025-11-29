e4df0fb5f41cd9596f26fcf8c9a7b7ae5b3e2f41
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-11-29 15:44:59 +0530

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import connectDB from "./configs/db.js";
-import userRouter from "./routes/userRoutes.js";
-import resumeRouter from "./routes/resumeRoutes.js";
-import aiRouter from "./routes/aiRoutes.js";
+import serverless from "serverless-http";
+import "dotenv/config.js";
+import connectDB from "../configs/db.js";
+import userRouter from "../routes/userRoutes.js";
+import resumeRouter from "../routes/resumeRoutes.js";
+import aiRouter from "../routes/aiRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Database Connection
+// Database
 await connectDB();
 
 app.use(express.json());
@@ -21,11 +21,14 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => res.send("Server is live... "));
+app.get("/", (req, res) => {
+  res.send("Backend working on Vercel!");
+});
+
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
 app.use("/api/ai", aiRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// ❗ No app.listen() in Vercel
+export const handler = serverless(app);
+export default app;
