43c8de10371290057ffd27b537c97ea882a3bd8d
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2025-11-29 15:51:57 +0530

    changes in server.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,25 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try{
-        mongoose.connection.on("connected",()=>{
-            console.log("Database Connected Successfully")
-        })
+  try {
+    let mongodbURI = process.env.MONGODB_URI;
+    const projectName = "resume-builder";
 
-        let mongodbURI = process.env.MONGODB_URI;
-        const projectName = 'resume-builder';
-        if(!mongodbURI){
-            throw new Error("MONGODB_URI environment variable not set")
-        }
+    if (!mongodbURI) throw new Error("MONGODB_URI not set");
 
-        if(mongodbURI.endsWith('/')){
-            mongodbURI = mongodbURI.slice(0,-1)
-        }
-
-        await mongoose.connect(`${mongodbURI}/${projectName}`)
-    } catch(error){
-        console.error("Error connecting to MongoDB:", error)
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
     }
-}
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+    console.log("MongoDB Connected ✔️");
+  } catch (error) {
+    console.error("MongoDB Error:", error.message);
+  }
+};
 
 export default connectDB;
