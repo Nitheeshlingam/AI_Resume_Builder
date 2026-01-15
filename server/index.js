f575191c9ab1a61356c4cfc622598ebe44238165
Nitheeshlingam <nitheeshlingam2024@gmail.com>
2026-01-15 11:40:27 +0530

    removed app listen in index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,3 @@ app.use('/api/users', userRouter)
 app.use('/api/resumes', resumeRouter)
 app.use('/api/ai', aiRouter)
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-
-})
